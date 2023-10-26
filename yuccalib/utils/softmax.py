41a7acb8d14dc668ce2e8f1e94ab48af4f5260d0
Asbjørn Munk <asbjorn@hey.com>
2023-10-26 16:34:02 +0200

    Fix test

--- yuccalib/utils/softmax.py
+++ yuccalib/utils/softmax.py
@@ -3,5 +3,5 @@ np.seterr(all="ignore")
 
 def softmax(x, axis=1):
     """Compute softmax values for each sets of scores in x."""
-    x_exp = np.exp(x - np.max(x, axis=1))
+    x_exp = np.exp(x - np.max(x, axis=axis))
     return x_exp / np.sum(x_exp, axis=axis)
